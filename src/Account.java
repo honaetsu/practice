50956b451b3a8196e31a58befedebc00a3f70c15
tips05 <tips05@192.168.1.105>
2018-07-31 20:40:03 +0900

    Accountクラス

---
+++ src/Account.java
@@ -0,0 +1,57 @@
+
+public class Account {
+
+	// メンバ
+	private String userId;
+	private String pass;
+	private String mail;
+	private String name;
+
+	public Account() {
+		super();
+	}
+
+	// アクセッサメソッド
+	private int age;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
