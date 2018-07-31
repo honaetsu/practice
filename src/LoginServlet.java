a16920f96f9bb58f5518783577b8ac4e4f4324e6
tips05 <tips05@192.168.1.105>
2018-07-31 21:00:56 +0900

    ログイン関連

---
+++ src/LoginServlet.java
@@ -0,0 +1,39 @@
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LoginServlet() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		res.getWriter().append("Served at: ").append(req.getContextPath());
+
+		// 遷移ページの指定
+		String page="login.jsp";
+		// フォワード処理
+		RequestDispatcher rd=req.getRequestDispatcher(page);
+
+	}
+
+}
