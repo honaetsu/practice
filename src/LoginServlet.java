a67a0d58be9db369987dcb8baea9c7a67f3162e5
ETSUKO <ETSUKO@ETSUKO-PC>
2018-08-02 22:48:23 +0900

    8/2etsuPc追加

--- src/LoginServlet.java
+++ src/LoginServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class LoginServlet
  */
-@WebServlet("/LoginServlet")
+@WebServlet("/Login")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
