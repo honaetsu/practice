6777af04f100fe77bab88fee0cb7694f1602cc00
tips05 <tips05@192.168.1.105>
2018-09-25 21:01:21 +0900

    次Isvalidメソッドを完成させること

--- src/AccountDAO.java
+++ src/AccountDAO.java
@@ -1,7 +1,8 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class AccountDAO {
 	private Connection con;
@@ -18,7 +19,7 @@ public class AccountDAO {
 
 	// 接続
 	public void connect() throws SQLException {
-		String url = "jdbc:mysql://" + server + "3306/test?characterEncoding=UTF-8&serverTimezone=JST";
+		String url = "jdbc:mysql://" + server + "3306/practice?characterEncoding=UTF-8&serverTimezone=JST";
 		con = DriverManager.getConnection(url, user, password);
 	}
 
@@ -31,12 +32,39 @@ public class AccountDAO {
 
 	// ログイン確認
 	public AccountBean getAccount(String pId) throws SQLException {
-		ResultSet res=null;
-		AccountBean abean=null;
+		Statement stmt = null;
+		ResultSet res = null;
+		AccountBean aBean = null;
 		// テーブルからID検索し、結果からPASSを検索する
 		String sql = "SELECT * FROM account WHERE PASS=(SELECT PASS FROM account WHERE USER_ID =" + pId + ")";
-		abean = new AccountBean();
-		return abean;
+		aBean = new AccountBean();
+		try {
+			// ステートメントの作成
+			stmt = con.createStatement();
+			// SQL実行,結果をresに
+			res = stmt.executeQuery(sql);
+			// 結果をアカウントオブジェクトにセットする
+			if(res.next()){
+				aBean.setUserId(res.getInt("USER_ID"));
+				aBean.setPass(res.getString("PASS"));
+				aBean.setMail(res.getString("MAIL"));
+				aBean.setName(res.getString("NAME"));
+				aBean.setAge(res.getInt("AGE"));
+			}
+			// ログイン可能か判断する
+			IsValid isv=new IsValid();
+			Boolean test=isv.isValidTest(aBean);
+//＞ココマデIsValidクラスを先に完成させること
+			// カーソルをResultSet オブジェクト内の最終行に移動
+			res.last();
+			// 現在の行の番号を取得
+			int count = res.getRow();
+			// sentou ni
+
+		} catch (SQLException e) {
+
+		}
+		return aBean;
 	}
 	//
 }
--- src/IsValid.java
+++ src/IsValid.java
@@ -2,19 +2,16 @@ import java.sql.SQLException;
 
 public class IsValid {
 
-	String pId;// メンバID
-	String pPass;// メンバPass
+
 
 	// コンストラクタ
-	public IsValid(String pId, String pPass) {
-		this.pId = pId;
-		this.pPass = pPass;
+	public IsValid() {
+
 	}
 
 	// ログイン可能かどうか評価するメソッド
-	public boolean isValidTest(AccountDAO dao) {
+	public boolean isValidTest(AccountBean aBean) {
 		try{
-		dao.connect();
 
 
 
