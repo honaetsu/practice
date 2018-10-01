10b0e27565282fc3575bd59c1b3df256fec3ba03
honaetsu <41929803+honaetsu@users.noreply.github.com>
2018-10-01 19:16:10 +0900

    エクスプローラからコピー

--- src/IsValid.java
+++ src/IsValid.java
@@ -1,26 +1,15 @@
-import java.sql.SQLException;
-
 public class IsValid {
 
-	String pId;// メンバID
-	String pPass;// メンバPass
+
 
 	// コンストラクタ
-	public IsValid(String pId, String pPass) {
-		this.pId = pId;
-		this.pPass = pPass;
+	public IsValid() {
+
 	}
 
 	// ログイン可能かどうか評価するメソッド
-	public boolean isValidTest(AccountDAO dao) {
-		try{
-		dao.connect();
+	public boolean isValidTest(AccountBean aBean) {
 
-
-
-		}catch(SQLException e){
-
-		}
 		return false;
 	}
 }
--- src/LoginServlet.java
+++ src/LoginServlet.java
@@ -46,16 +46,16 @@ public class LoginServlet extends HttpServlet {
 			dao.connect();
 			// ログイン可能か確認する
 			// IsValidクラスのnew
-			IsValid ivd = new IsValid(pId, pPass);
-			boolean test = ivd.isValidTest(dao);
-			if (test) {
-				// ログイン成功時の画面遷移先ページの指定
-				page = "loginOk.jsp";
-				// リクエストオブジェクトに
-
-			} else {
-
-			}
+//			IsValid ivd = new IsValid(pId, pPass);
+//			boolean test = ivd.isValidTest(dao);
+//			if (test) {
+//				// ログイン成功時の画面遷移先ページの指定
+//				page = "loginOk.jsp";
+//				// リクエストオブジェクトに
+//
+//			} else {
+//
+//			}
 		} catch (SQLException e) {
 
 		}
